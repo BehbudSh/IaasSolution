d5b7763a31551fab9317835a7f9db02424886e48
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-05-29 17:00:41 +0400

    Changing script

--- securestring.ps1
+++ securestring.ps1
@@ -1,22 +1,4 @@
-$resourceGrouplocation = 'West Europe'
-$resourceGroupName = 'contoso-IaaS'
-$resourceDeploymentName = 'contoso-IaaS-deployment'
-$template = 'contosoIaaS.json'
 $password = "B123456b!" 
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
-
-### Create Resource Group
-New-AzureRmResourceGroup `
-    -Name $resourceGroupName `
-    -Location $resourcegrouplocation `
-    -Verbose -Force
-    
-### Deploy Resources
 $additionalParameters = New-Object -TypeName hashtable
 $additionalParameters['vmAdminPassword'] = $securePassword
-New-AzureRmResourceGroupDeployment `
-    -Name $resourceDeploymentName `
-    -ResourceGroupName $resourceGroupName `
-    -TemplateFile $template `
-    @additionalParameters `
-    -Verbose -Force
